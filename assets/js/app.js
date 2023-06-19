418714831d6529588951fd25fbb3c819a3dea887
Hugo Scelers <hugo.scelers@outlook.com>
2023-06-19 15:12:50 +0200

    front v3

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,29 @@
 const jokeButton = document.getElementById('jokeButton');
+const answerButton = document.getElementById('answerButton');
 const jokeText = document.getElementById('jokeText');
-const answerText = document.getElementById('answerText'); // Ajouter une nouvelle balise HTML avec cet ID pour afficher la réponse
+const answerText = document.getElementById('answerText');
+
+let currentJoke = null;
 
 const fetchRandomJoke = () => {
     fetch('https://carambar.onrender.com/jokes/random')
         .then((response) => response.json())
         .then((data) => {
+            currentJoke = data;
             jokeText.innerText = data.joke;
-            answerText.innerText = data.answer; // affiche la réponse
+            answerButton.style.display = 'block';
+            answerText.style.display = 'none';
+            answerText.innerText = '';
         })
         .catch((error) => {
             console.log(error);
         });
 };
 
+const showAnswer = () => {
+    answerText.innerText = currentJoke.answer;
+    answerText.style.display = 'block';
+};
+
 jokeButton.addEventListener('click', fetchRandomJoke);
+answerButton.addEventListener('click', showAnswer);
